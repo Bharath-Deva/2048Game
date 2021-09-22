70dfc763afcf4ba62ebd47873df53e9f73604e51
Bharath Deva <dbharath27@gmail.com>
2021-09-23 01:23:12 +0530

    responsive bugs fixed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,10 @@ const Header = ({ score, bestScore }) => {
   return (
     <div className="App-header container-fluid">
       <div className="row justify-content-center">
-        <div className="col col-lg-2 col-md-2 col-sm-2">
+        <div className="col-6 col-lg-2 col-md-4 col-sm-4">
           <h5 className="btn btn-primary score-card">Score : {score}</h5>
         </div>
-        <div className="col-md-auto col-sm-2"></div>
-        <div className="col col-lg-2 col-md-2 col-sm-2">
+        <div className="col-6 col-lg-2 col-md-4 col-sm-4">
           <h5 className="btn btn-primary new-game-btn" >Best Score : {bestScore}</h5>
         </div>
       </div>
--- src/components/Table.js
+++ src/components/Table.js
@@ -17,58 +17,58 @@ const Table = ({ grid }) => {
   return (
     <div className="container-fluid">
       <div className="d-flex row justify-content-center">
-        <div style={{ backgroundColor: styleForBlocks[grid[0][0]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[0][0]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[0][0] !== '0' ? grid[0][0] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[0][1]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[0][1]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[0][1] !== '0' ? grid[0][1] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[0][2]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[0][2]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[0][2] !== '0' ? grid[0][2] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[0][3]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[0][3]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[0][3] !== '0' ? grid[0][3] : ''}</p>
         </div>
       </div>
       <div className="d-flex row justify-content-center">
-        <div style={{ backgroundColor: styleForBlocks[grid[1][0]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[1][0]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[1][0] !== '0' ? grid[1][0] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[1][1]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[1][1]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[1][1] !== '0' ? grid[1][1] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[1][2]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[1][2]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[1][2] !== '0' ? grid[1][2] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[1][3]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[1][3]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[1][3] !== '0' ? grid[1][3] : ''}</p>
         </div>
       </div>
       <div className="d-flex row justify-content-center">
-        <div style={{ backgroundColor: styleForBlocks[grid[2][0]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[2][0]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[2][0] !== '0' ? grid[2][0] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[2][1]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[2][1]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[2][1] !== '0' ? grid[2][1] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[2][2]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[2][2]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[2][2] !== '0' ? grid[2][2] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[2][3]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[2][3]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[2][3] !== '0' ? grid[2][3] : ''}</p>
         </div>
       </div>
       <div className="d-flex row justify-content-center">
-        <div style={{ backgroundColor: styleForBlocks[grid[3][0]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[3][0]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[3][0] !== '0' ? grid[3][0] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[3][1]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[3][1]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[3][1] !== '0' ? grid[3][1] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[3][2]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[3][2]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[3][2] !== '0' ? grid[3][2] : ''}</p>
         </div>
-        <div style={{ backgroundColor: styleForBlocks[grid[3][3]] }} className="block col-lg-1 col-md-2 col-sm-2">
+        <div style={{ backgroundColor: styleForBlocks[grid[3][3]] }} className="block col-3 col-lg-1 col-md-2 col-sm-2">
           <p className="blockElement">{grid[3][3] !== '0' ? grid[3][3] : ''}</p>
         </div>
       </div>
